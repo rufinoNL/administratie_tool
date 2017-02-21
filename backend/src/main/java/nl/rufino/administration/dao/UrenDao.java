8b4f9d98194f2e891e1cdedb592caf79d3a392de
Jonathan Ramos <rufino@rufino.nl>
2017-02-21 17:56:14 +0100

    JR0004: UrenDao MongoDB

--- backend/src/main/java/nl/rufino/administration/dao/UrenDao.java
+++ backend/src/main/java/nl/rufino/administration/dao/UrenDao.java
@@ -1,7 +1,36 @@
 package nl.rufino.administration.dao;
 
+import java.util.List;
+
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+import org.mongodb.morphia.geo.GeoJson;
+import org.mongodb.morphia.geo.Point;
+
+import com.mongodb.MongoClient;
+
+import nl.rufino.administration.model.Uren;
+
 public class UrenDao {
 	private static final String ADMINISTRATION_DB = "administration";
+	private static Datastore datastore;
+
+	public UrenDao(){
+		init();
+	}
+
+	private void init() {
+		if(datastore == null){
+			Morphia morphia = new Morphia();
+			datastore = morphia.createDatastore(new MongoClient(), ADMINISTRATION_DB);
+			System.out.println("Datastore geinitialiseerd..");
+		}
+	}
 	
+	public List<Uren> haalUrenOpVanKlant(String zoekWaarde) {
+		datastore.ensureIndexes();
+		List<Uren> queryResult = datastore.createQuery(Uren.class).field("klant").contains(zoekWaarde).asList();		
+		return queryResult;
+	}
 
 }
