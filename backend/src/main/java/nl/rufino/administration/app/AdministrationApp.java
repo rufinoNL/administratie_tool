cadbdaabaa6f06d4b4718c63a13dad8e0e1402b3
Jonathan Ramos <rufino@rufino.nl>
2017-02-21 18:05:03 +0100

    JR0004: applicatie toegevoegd met pad

---
+++ backend/src/main/java/nl/rufino/administration/app/AdministrationApp.java
@@ -0,0 +1,9 @@
+package nl.rufino.administration.app;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class AdministrationApp extends Application{
+
+}
