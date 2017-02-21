8640398af7a62aa05c8b86d1bcff5ca364317587
Jonathan Ramos <rufino@rufino.nl>
2017-02-21 08:11:13 +0100

    Initiele backend dev

---
+++ backend/src/main/java/nl/rufino/administration/dao/UrenDao.java
@@ -0,0 +1,7 @@
+package nl.rufino.administration.dao;
+
+public class UrenDao {
+	private static final String ADMINISTRATION_DB = "administration";
+	
+
+}
---
+++ backend/src/test/java/nl/rufino/administration/dao/UrenDaoIT.java
@@ -0,0 +1,14 @@
+package nl.rufino.administration.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UrenDaoIT {
+
+	@Test
+	public void when_dataWrittenToUrenCollection_then_urenSaved() {
+		
+	}
+
+}
