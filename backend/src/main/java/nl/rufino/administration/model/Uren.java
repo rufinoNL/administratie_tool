4ed09e892d42009b2b30b59de2b47663e09562a6
Jonathan Ramos <rufino@rufino.nl>
2017-02-23 01:30:17 +0100

    JR0004: model uitgebreid voor de uren.

--- backend/src/main/java/nl/rufino/administration/model/Uren.java
+++ backend/src/main/java/nl/rufino/administration/model/Uren.java
@@ -1,6 +1,9 @@
 package nl.rufino.administration.model;
 
+import java.math.BigDecimal;
+
 import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Embedded;
 import org.mongodb.morphia.annotations.Entity;
 import org.mongodb.morphia.annotations.Id;
 import org.mongodb.morphia.annotations.Property;
@@ -9,32 +12,54 @@ import org.mongodb.morphia.annotations.Property;
 public class Uren {
 	@Id
 	private ObjectId _id;
+	@Property("datum")
+	private String datum;
 	@Property("klant")
 	private String klant;
+	@Embedded
+	private Opdracht opdracht;
 	@Property("uren")
 	private int uren;
-	
+	@Property("tarief")
+	private BigDecimal tarief;
+
 	public int getUren() {
 		return uren;
 	}
-	
+
 	public void setUren(int uren) {
 		this.uren = uren;
 	}
-	
+
 	public String getKlant() {
 		return klant;
 	}
-	
+
 	public void setKlant(String klant) {
 		this.klant = klant;
 	}
-	
+
 	public ObjectId get_id() {
 		return _id;
 	}
-	
+
 	public void set_id(ObjectId _id) {
 		this._id = _id;
+	}
+
+	public Opdracht getOpdracht() {
+		return opdracht;
+	}
+
+	public void setOpdracht(Opdracht opdracht) {
+		this.opdracht = opdracht;
+	}
+
+	public BigDecimal getTarief() {
+		return tarief;
+	}
+
+	public void setTarief(BigDecimal tarief) {
+		this.tarief = tarief;
 	}
 }
