3d661399afc4832bc333536919c6bc3799185a94
Jonathan Ramos <rufino@rufino.nl>
2017-02-24 00:18:16 +0100

    JR0005: datums worden gecheckt op voorloopnullen

--- backend/src/main/java/nl/rufino/administration/resources/UrenResourceImpl.java
+++ backend/src/main/java/nl/rufino/administration/resources/UrenResourceImpl.java
@@ -2,6 +2,7 @@ package nl.rufino.administration.resources;
 
 import java.util.List;
 
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.core.Response;
 
 import nl.rufino.administration.dao.UrenDao;
@@ -29,9 +30,27 @@ public class UrenResourceImpl implements UrenResource{
 
 	@Override
 	public Response urenBinnenPeriode(String vanaf, String totenmet) {
-		List<Uren> urenBinnenPeriode = urenDao.urenBinnenPeriode(vanaf, totenmet);
+		String checkedVanaf = checkDatum(vanaf);
+		String checkedTotenmet= checkDatum(totenmet);
+		
+		List<Uren> urenBinnenPeriode = urenDao.urenBinnenPeriode(checkedVanaf, checkedTotenmet);
 		
 		return Response.ok().entity(urenBinnenPeriode).build();
+	}
+
+	private String checkDatum(String datum) throws BadRequestException{
+		String[] splitDatum = datum.split("-");
+		
+		if(splitDatum.length!=3 || datum.length() < 8 || datum.length() > 10){
+			throw new BadRequestException("Datum behoort uit drie delen te bestaan in de vorm: yyyy-(M)M-(d)d. Min lengte = 8, max lengte = 10.");
+		}
+
+		for(int i = 1;i<3;i++){
+			if(splitDatum[i].length()>0 && splitDatum[i].length()<2){
+				splitDatum[i] = "0" + splitDatum[i];
+			}
+		}
+		return splitDatum[0] + "-" + splitDatum[1] + "-" + splitDatum[2];
 	}
 
 }
